bf0c89ae26ae7772a343dc29b2f8993b9bb45d66
Dan <dd8556205@gmail.com>
2024-09-07 22:38:19 +0000

    add logout method + return types

--- src/features/auth/auth.controller.ts
+++ src/features/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Inject, UseGuards } from '@nestjs/common';
+import { Controller, Get, Inject, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { GoogleAuthGuard, GoogleVerificationGuard } from 'src/common/guards';
 import { GoogleOAuthUser } from './auth.dto';
 import { CurrentUser } from 'src/common/decarators';
 import { User } from '@prisma/client';
 import { AUTH_SERVICE_TOKEN } from 'src/common/constants';
+import { Request } from 'express';
 
 @Controller()
 export class AuthController {
@@ -18,13 +19,21 @@ export class AuthController {
 
   @Get('auth/google/callback')
   @UseGuards(GoogleAuthGuard)
-  async googleAuthRedirect(@CurrentUser() user: GoogleOAuthUser) {
+  async googleAuthRedirect(
+    @CurrentUser() user: GoogleOAuthUser,
+  ): Promise<GoogleOAuthUser> {
     return user;
   }
 
   @Get('stats')
   @UseGuards(GoogleVerificationGuard)
-  async getUser(@CurrentUser() user: User) {
+  async getUser(@CurrentUser() user: User): Promise<User | string> {
     return user ? user : 'clg';
+  }
+
+  @Get('logout')
+  @UseGuards(GoogleVerificationGuard)
+  async logout(@Req() req: Request) {
+    return await this.authService.logout(req);
   }
 }
