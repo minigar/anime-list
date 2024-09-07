36c7d85865fb5fa38a27eaee9be70dc3da19cb94
Dan <dd8556205@gmail.com>
2024-09-07 00:08:03 +0000

    add hemlet & configService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {});
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('APP_PORT', 3001);
+
+  app.use(helmet.dnsPrefetchControl());
+  app.use(helmet.frameguard());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.ieNoOpen());
+  app.use(helmet.noSniff());
+  app.use(helmet.referrerPolicy());
+  app.use(helmet.xssFilter());
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port);
+  Logger.log(`Server has been started at ${port} port!`);
 }
+
 bootstrap();
