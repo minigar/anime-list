d7285c037312d1d778aed68eed4382d3c1c7f85b
Dan <dd8556205@gmail.com>
2024-09-10 19:38:08 +0000

    add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './features/auth/auth.module';
 import { TitleModule } from './features/titles/titles.module';
+import { seconds, ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, TitleModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: seconds(config.getOrThrow<number>('UPLOAD_RATE_TTL_SHORT')),
+          limit: config.getOrThrow<number>('UPLOAD_RATE_LIMIT_SHORT'),
+        },
+      ],
+    }),
+    AuthModule,
+    TitleModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
